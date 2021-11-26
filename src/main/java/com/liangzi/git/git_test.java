151c6c6e777d84c619572d8e812b95b1d2e16591
liangzi <564403260@qq.com>
2021-11-26 18:34:24 +0800

    secoud commit

--- src/main/java/com/liangzi/git/git_test.java
+++ src/main/java/com/liangzi/git/git_test.java
@@ -7,5 +7,6 @@ package com.liangzi.git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
