a8d307c6c58e6edb5715ffe9cc5b171888e4e6e9
liangzi <564403260@qq.com>
2021-11-26 18:35:07 +0800

    third commit

--- src/main/java/com/liangzi/git/git_test.java
+++ src/main/java/com/liangzi/git/git_test.java
@@ -8,5 +8,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
